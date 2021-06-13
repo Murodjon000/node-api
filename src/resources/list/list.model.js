8774a9f7da0235e675e5f80933bf493ddd873653
Murodjon <murodjon.tursunpulatov@gmail.com>
2021-06-13 16:17:29 +0500

    add list model

---
+++ src/resources/list/list.model.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+const listSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    maxlength: 50,
+  },
+
+  description: String,
+  createdBy: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+})
+
+listSchema.index({ user: 1, name: 1 }, { unique: true })
+
+export const List = mongoose.model('list', listSchema)
